fdea791203718139605d26a5b1357cf46cf4831e
longnh <longjava2024@gmail.com>
2024-11-02 15:17:05 +0700

    fix image

--- src/main/java/com/example/imageservice/config/WebConfig.java
+++ src/main/java/com/example/imageservice/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.example.imageservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,5 +16,17 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/**")
                 .addResourceLocations("file:uploads/");
     }
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
+            }
+        };
+    }
+
 
 }
